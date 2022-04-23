aa3c3f08b6781a26db70edcb93e01fae75f4e14b
Yoshiyuki KOBAYASHI <kbysy1em@gmail.com>
2022-04-23 21:31:35 +0900

    Initial commit

---
+++ pdf_utils.py
@@ -0,0 +1,141 @@
+#import sys
+import os
+import PyPDF2
+
+class Feature:
+    def execute(self):
+        pass
+
+#==============================================================================
+class MergeFeature(Feature):
+    def __init__(self):
+        self.merger = PyPDF2.PdfFileMerger()
+
+    def execute(self):
+        pass
+
+class MergeOneByOneFeature(MergeFeature):
+    def execute(self):
+        print('Input filename, enter blank line to finish')
+        self.merge_recursively(self.merger)
+
+        # 挿入の場合はこちら(挿入したいページを0始まりで入力)
+        #merger.merge(2, '')
+
+        self.merger.write('merged.pdf')
+        self.merger.close()
+
+    def merge_recursively(self, m):
+        file_name = input('File name? ')
+        if file_name == '':
+            return
+
+        m.append(file_name)
+        self.merge_recursively(m)
+
+class MergeListFeature(MergeFeature):
+    def execute(self):
+        with open('./merge_list.txt', 'rt', encoding='utf-8') as f:
+            self.lines = f.read().splitlines()
+
+        self.merge_recursively(self.merger)
+
+        # 挿入の場合はこちら(挿入したいページを0始まりで入力)
+        #merger.merge(2, '')
+
+        self.merger.write('merged.pdf')
+        self.merger.close()
+        
+    def merge_recursively(self, m):
+        if self.lines == []:
+            return
+        print('merged file: ' + self.lines[0])
+        m.append(self.lines.pop(0))
+        self.merge_recursively(m)
+
+#==============================================================================
+class RotateFeature(Feature):
+    def execute(self):
+        filename = input('File name?')
+        file = PyPDF2.PdfFileReader(open(filename, 'rb'))
+
+        # pageは1はじまりとする
+        print('Input pages rotated (comma separated). If blank, all pages are rotated')
+        s = input()
+        if not s:
+            rotate_pages = range(1, file.numPages + 1)
+        else:
+            rotate_pages = [int(x.strip()) for x in s.split(',')]
+
+        direction = input('Clockwise(y/n)?')
+        if direction == 'n':
+            rotation = AntiClockwiseRotation()
+        else:
+            rotation = ClockwiseRotation()
+
+        self.output = PyPDF2.PdfFileWriter()
+        for i in range(file.numPages):
+            page = file.getPage(i)
+            if i + 1 in rotate_pages:
+                rotation.rotate(page)
+            self.output.addPage(page)
+
+        with open('rot-' + filename, 'wb') as f:
+            self.output.write(f)
+
+class Rotation:
+    def rotate(self, page):
+        pass
+
+class ClockwiseRotation(Rotation):
+    def rotate(self, page):
+        page.rotateClockwise(90)
+
+class AntiClockwiseRotation(Rotation):
+    def rotate(self, page):
+        page.rotateClockwise(-90)
+#==============================================================================
+class ExtractFeature(Feature):
+    def execute(self):
+        filename = input('File name?')
+
+        merger = PyPDF2.PdfFileMerger()
+
+        print('If you want pages of 4th ~ 6th, input 4 and 6')
+        start = int(input('Start page? '))
+        end = int(input('End page? '))
+        merger.append(filename, pages=(start - 1, end))
+
+        # 以下ではstart=2, stop=3としている。
+        # この場合、3ページ以降で4ページより前の部分が取り出され,
+        # 結果的に3ページ目のみのファイルが得られる
+        # merger.append(filename, pages=(2,3))
+
+        # n = merger.getNumPages()
+        # merger.append(input_file_name, pages=(0,n))
+
+        merger.write('extracted.pdf')
+
+        merger.close()
+
+#==============================================================================
+def main():
+    feature_num = input('[1]: Merge, [2]: Rotate, [3]: Extract? ')
+
+    if feature_num == '1':
+        if os.path.exists('./merge_list.txt'):
+            feature = MergeListFeature()
+        else:
+            feature = MergeOneByOneFeature()
+    elif feature_num == '2':
+        feature = RotateFeature()
+    elif feature_num == '3':
+        feature = ExtractFeature()
+    else:
+        print('No feature selected')
+        return
+
+    feature.execute()
+
+if __name__ == '__main__':
+    main()
